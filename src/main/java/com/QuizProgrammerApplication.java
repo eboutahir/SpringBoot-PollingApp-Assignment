aaf35bd920a080398fc93201ceac84eec2724f7b
imane1 <bouassel.imane1@gmail.com>
2023-10-26 00:58:06 +0000

    Polling app

---
+++ src/main/java/com/QuizProgrammerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizProgrammerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizProgrammerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizprogrammer/QuizProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizprogrammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizProgrammerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
