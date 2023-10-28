ac3b91ae47f623f4c92c9697550ad2ddb473c0fe
imane1 <bouassel.imane1@gmail.com>
2023-10-28 11:59:10 +0000

    Polling appp

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -16,56 +16,56 @@ import java.util.List;
 
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	Result result;
 	@Autowired
 	QuizService qService;
-	
+
 	Boolean submitted = false;
-	
+
 	@ModelAttribute("result")
 	public Result getResult() {
 		return result;
 	}
-	
+
 	@GetMapping("/")
 	public String home() {
 		return "loa.html";
 	}
-	
+
 	@PostMapping("/quiz")
 	public String quiz(@RequestParam String username, Model m, RedirectAttributes ra) {
 		if(username.equals("")) {
 			ra.addFlashAttribute("warning", "You must enter your name");
 			return "redirect:/";
 		}
-		
+
 		submitted = false;
 		result.setUsername(username);
-		
+
 		QuestionForm qForm = qService.getQuestions();
 		m.addAttribute("qForm", qForm);
-		
+
 		return "quiz.html";
 	}
-	
+
 	@PostMapping("/submit")
 	public String submit(@ModelAttribute QuestionForm qForm, Model m) {
 		if(!submitted) {
 			result.setTotalCorrect(qService.getResult(qForm));
-			qService.saveScore((javax.xml.transform.Result) result);
+			qService.saveScore(result);
 			submitted = true;
 		}
-		
+
 		return "result.html";
 	}
-	
+
 	@GetMapping("/score")
 	public String score(Model m) {
-		List<javax.xml.transform.Result> sList = qService.getTopScore();
+		List<Result> sList = qService.getTopScore();
 		m.addAttribute("sList", sList);
-		
+
 		return "scoreboard.html";
 	}
 
--- src/main/java/com/service/QuizService.java
+++ src/main/java/com/service/QuizService.java
@@ -2,38 +2,37 @@ package com.service;
 
 import com.model.Question;
 import com.model.QuestionForm;
+import com.model.Result;
 import com.repository.QuestionRepo;
 import com.repository.ResultRepo;
-import org.aspectj.weaver.patterns.TypePatternQuestions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import javax.xml.transform.Result;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 @Service
 public class QuizService {
-	
+
 	@Autowired
-	TypePatternQuestions.Question question;
+	Question question;
 	@Autowired
 	QuestionForm qForm;
 	@Autowired
-	QuestionRepo qRepo;
+	public QuestionRepo qRepo;
 	@Autowired
 	Result result;
 	@Autowired
 	ResultRepo rRepo;
-	
+
 	public QuestionForm getQuestions() {
 		List<Question> allQues = qRepo.findAll();
 		List<Question> qList = new ArrayList<Question>();
-		
+
 		Random random = new Random();
-		
+
 		for(int i=0; i<5; i++) {
 			int rand = random.nextInt(allQues.size());
 			qList.add(allQues.get(rand));
@@ -41,30 +40,30 @@ public class QuizService {
 		}
 
 		qForm.setQuestions(qList);
-		
+
 		return qForm;
 	}
-	
+
 	public int getResult(QuestionForm qForm) {
 		int correct = 0;
-		
+
 		for(Question q: qForm.getQuestions())
 			if(q.getAns() == q.getChose())
 				correct++;
-		
+
 		return correct;
 	}
-	
+
 	public void saveScore(Result result) {
 		Result saveResult = new Result();
 		saveResult.setUsername(result.getUsername());
 		saveResult.setTotalCorrect(result.getTotalCorrect());
 		rRepo.save(saveResult);
 	}
-	
+
 	public List<Result> getTopScore() {
 		List<Result> sList = rRepo.findAll(Sort.by(Sort.Direction.DESC, "totalCorrect"));
-		
+
 		return sList;
 	}
 }
